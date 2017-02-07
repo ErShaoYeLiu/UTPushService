56621cd3752056da732b2bd7320d72857c9dd0e1
陈凤林 <feng-lin.chen@utsoft.cn>
2017-02-07 13:12:57 +0800

    ..

--- UTPushService/ViewController.m
+++ UTPushService/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"aa");
+    self.view.backgroundColor = [UIColor yellowColor];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
